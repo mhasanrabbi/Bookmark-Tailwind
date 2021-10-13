a445e8ec61b671093386f4a5b53f5e8c152c6dfc
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2021-10-13 14:53:46 +0600

    Tabs

--- public/app.js
+++ public/app.js
@@ -16,3 +16,28 @@ close.addEventListener('click', (e) => {
   mobileNav.classList.toggle('show');
   close.classList.add('click');
 });
+
+// Tabs
+
+const tabs = document.querySelectorAll('[data-tab-target]');
+const tabContents = document.querySelectorAll('[data-tab-content]');
+
+tabs.forEach((tab) => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget);
+    tabContents.forEach((tabContents) => {
+      tabContents.classList.remove('active');
+    });
+    target.classList.add('active');
+  });
+});
+
+// FAQ Accordion
+let item = document.querySelectorAll('[data-faq-item]');
+let i;
+
+for (i = 0; i < item.length; i++) {
+  item[i].addEventListener('click', function () {
+    this.classList.toggle('open');
+  });
+}
